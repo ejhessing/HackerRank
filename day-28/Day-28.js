9ed28c3ccba86cee025648db2bf7beb05d6cb7a1
ejhessing <ejhessing@gmail.com>
2016-11-16 05:11:04 +0000

    Added day-28's JS Code

---
+++ day-28/Day-28.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var N = parseInt(readLine());
+    var arr = []
+    for(var a0 = 0; a0 < N; a0++){
+        var firstName_temp = readLine().split(' ');
+        var firstName = firstName_temp[0];
+        var emailID = firstName_temp[1];
+        
+        //emailID.endsWith('@gmail.com') - Another way to find just valid gmail email addresses
+        if((/\w+([.-]?\w+)*@gmail.com/).test(emailID)) {
+          arr.push(firstName)
+        }
+    }
+    console.log(arr.sort().join('\n'))
+
+
+}
+
+
